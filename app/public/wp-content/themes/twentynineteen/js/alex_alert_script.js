5ed864a67bea2e59e2b85c74951f6f1e862444ae
oleksiimosevych <oleksii.mosevych@gmail.com>
2021-05-23 14:32:51 +0300

    js working alert and console.log ok

--- app/public/wp-content/themes/twentynineteen/js/alex_alert_script.js
+++ app/public/wp-content/themes/twentynineteen/js/alex_alert_script.js
@@ -1,11 +1,15 @@
 window.onload = init;
 
-function init(){
+function init(alex_text){
     var button = document.getElementById("alex_button")
     button.onclick = handleButtonClick;
+	console.log(alex_text);
 }
 
-function handleButtonClick() {
-	alert('Hello world!');
+function handleButtonClick(alex_text) {
+	alex_text="Hello world!";
+	init(alex_text);
+	alert(alex_text);
+
 }
 
