48451a93c72f414a9db68947958393c2f1955991
oleksiimosevych <oleksii.mosevych@gmail.com>
2021-05-23 15:59:54 +0300

    added flush, added taxonomy and added it to an admin submenu

--- app/public/wp-content/plugins/hello-world/hello-world.php
+++ app/public/wp-content/plugins/hello-world/hello-world.php
@@ -43,4 +43,89 @@ function add_theme_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+
+
+
+
+function create_taxonomy2(){
+	register_taxonomy( 'hello', [ 'hello' ], [
+		'label'                 => '', // определяется параметром $labels->name
+		'labels'                => [
+			'name'              => 'Helloes',
+			'singular_name'     => 'Hello',
+			'search_items'      => 'Search Helloes',
+			'all_items'         => 'All Helloes',
+			'view_item '        => 'View Hello',
+			'parent_item'       => 'Parent Hello',
+			'parent_item_colon' => 'Parent Hello:',
+			'edit_item'         => 'Edit Hello',
+			'update_item'       => 'Update Hello',
+			'add_new_item'      => 'Add New Hello',
+			'new_item_name'     => 'New Hello Name',
+			'menu_name'         => 'Hello',
+		],
+		'description'           => '', // описание таксономии
+		'public'                => true,
+		// 'publicly_queryable'    => null, // равен аргументу public
+		// 'show_in_nav_menus'     => true, // равен аргументу public
+		// 'show_ui'               => true, // равен аргументу public
+		// 'show_in_menu'          => true, // равен аргументу show_ui
+		// 'show_tagcloud'         => true, // равен аргументу show_ui
+		// 'show_in_quick_edit'    => null, // равен аргументу show_ui
+		'hierarchical'          => false,
+
+		'rewrite'               => true,
+		//'query_var'             => $taxonomy, // название параметра запроса
+		'capabilities'          => array(),
+		'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
+		'show_admin_column'     => true, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
+		'show_in_rest'          => null, // добавить в REST API
+		'rest_base'             => null // $taxonomy
+		// '_builtin'              => false,
+		//'update_count_callback' => '_update_post_term_count',
+	] );
+		flush_rewrite_rules();
+
+
+}
+add_action('init', 'create_taxonomy2');
+
+
+// REGISTER NEW POST TYPE
+// The plugin should register a custom post type “Hello” that should appear in the main WordPress menu with the “carrot” icon (use Dashicons).
+add_action('init', 'my_custom_init');
+function my_custom_init(){
+	register_post_type('hello', array(
+		'labels'             => array(
+			'name'               => 'Hellos', // Основное название типа записи
+			'singular_name'      => 'Hello', // отдельное название записи типа Book
+			'add_new'            => 'Добавить новую',
+			'add_new_item'       => 'Add new hi',
+			'edit_item'          => 'Edit hi',
+			'new_item'           => 'new hi',
+			'view_item'          => 'view hi',
+			'search_items'       => 'search hi',
+			'not_found'          => 'No hellos found',
+			'not_found_in_trash' => 'No hi in trash',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Hellos'
+		  ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,//in the main wp menu
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 1,
+		'menu_icon'			 => 'dashicons-carrot',//added carrot here
+
+		'supports'           => array('title','editor','author','thumbnail','excerpt','comments')
+	) );
+}
+
+
+
 ?>
